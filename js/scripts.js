55fc858c378d5a1f99db19730697f97686e9b91c
Paul-Ngigi <paulkush7777@gmail.com>
2021-02-18 11:07:15 +0300

    add a subtraction function to the page

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,10 @@ var num1 = parseInt(prompt("Enter a number: "))
 var num2 = parseInt(prompt("Enter another number: "))
 var result = add(num1,num2);
 alert("The sum of your numbers is " + result);
+
+// Subtraction
+var subtract = function(num1,num2){
+    return num1 - num2;
+};
+var subtract_result = num1 - num2;
+alert("The difference of your numbers is " + subtract_result); 
