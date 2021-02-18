77519cc7c891978d1eb32e781d5f455a476f3acc
Paul-Ngigi <paulkush7777@gmail.com>
2021-02-18 11:09:19 +0300

    add a multiple function to the page

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,10 @@ var subtract = function(num1,num2){
 };
 var subtract_result = num1 - num2;
 alert("The difference of your numbers is " + subtract_result); 
+
+// Multipliction
+var multiply = function(num1,num2){
+    return num1 * num2;
+};
+var multiply_result = num1 * num2;
+alert("The multiple of your numbers is " + multiply_result); 
